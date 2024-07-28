a6c8010b9555c38e93a189ee33279ebb7bb5795b
ZzinB <mysteleee7@gmail.com>
2024-07-28 21:46:45 +0900

    Add: 모든 페이지 접근 가능하도록 조정 (추후 수정 필요)
    SpringSecurity 안걸리도록 !

--- src/main/java/com/example/Wanted/Market/API/config/SecurityConfig.java
+++ src/main/java/com/example/Wanted/Market/API/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.Wanted.Market.API.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -18,27 +21,62 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${remember.me.key}")
+    private String rememberMeKey;
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/auth/**", "/payment/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/auth/**", "/payment/**", "/**").permitAll()
                         .anyRequest().authenticated()
                 )
-//                .formLogin(formLogin ->
-//                formLogin
-//                    .loginPage("/login")
-//                    .defaultSuccessUrl("/", true)
-//                    .permitAll()
-//            )
-                .logout(withDefaults());
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/login")
+                                .defaultSuccessUrl("/", true)
+                                .permitAll()
+                )
+                .logout(logout -> logout
+                        .permitAll()
+                )
+                .rememberMe(rememberMe ->
+                        rememberMe
+                                .tokenRepository(inMemoryTokenRepository())
+                                .tokenValiditySeconds(86400)
+                                .key(rememberMeKey) // 환경 변수에서 비밀 키 가져오기
+                                .userDetailsService(userDetailsService())
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                                .maximumSessions(1)
+                                .expiredUrl("/login?expired")
+                );
 
         return http.build();
     }
 
     @Bean
+    public InMemoryTokenRepositoryImpl inMemoryTokenRepository() {
+        return new InMemoryTokenRepositoryImpl();
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
