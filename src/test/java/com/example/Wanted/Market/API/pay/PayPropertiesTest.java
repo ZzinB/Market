dedd872c711495a48019de7e340c9deac907420d
ZzinB <mysteleee7@gmail.com>
2024-08-01 21:47:17 +0900

    Test: kakaopay secretkey 테스트

--- src/test/java/com/example/Wanted/Market/API/pay/PayPropertiesTest.java
+++ src/test/java/com/example/Wanted/Market/API/pay/PayPropertiesTest.java
@@ -1,5 +1,6 @@
 package com.example.Wanted.Market.API.pay;
 
+import com.example.Wanted.Market.API.pay.kakaopay.KakaoPayProperties;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
