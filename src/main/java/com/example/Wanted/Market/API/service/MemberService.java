8b911f806365e3e0e8245dcc161ee31c967c4ee0
ZzinB <mysteleee7@gmail.com>
2024-08-01 21:46:11 +0900

    Add: MemberDto

--- src/main/java/com/example/Wanted/Market/API/service/MemberService.java
+++ src/main/java/com/example/Wanted/Market/API/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.Wanted.Market.API.service;
 
 import com.example.Wanted.Market.API.domain.Member;
+import com.example.Wanted.Market.API.dto.MemberDto;
 
 public interface MemberService {
     //void signup(Member member);
@@ -11,6 +12,6 @@ public interface MemberService {
 //
 //    Member getUserByEmail(String email);
 
-    Member registerMember (Member member);
-
+    Member registerMember (MemberDto memberDto);
+    boolean authenticate(String username, String password);
 }
