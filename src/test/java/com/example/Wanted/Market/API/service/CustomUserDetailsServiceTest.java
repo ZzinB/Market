af2cba964d2a2520887ddaebb790e01d00a3ef8c
ZzinB <mysteleee7@gmail.com>
2024-08-01 21:44:04 +0900

    Test: loadUserByUsername

--- src/test/java/com/example/Wanted/Market/API/service/CustomUserDetailsServiceTest.java
+++ src/test/java/com/example/Wanted/Market/API/service/CustomUserDetailsServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.Wanted.Market.API.service;
 
+import com.example.Wanted.Market.API.domain.Address;
 import com.example.Wanted.Market.API.domain.Member;
+import com.example.Wanted.Market.API.domain.Role;
 import com.example.Wanted.Market.API.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class CustomUserDetailsServiceTest {
@@ -22,15 +25,31 @@ public class CustomUserDetailsServiceTest {
 
     @Test
     void testLoadUserByUsername() {
+        // Given
+//        Address address = new Address();
+//        address.setStreet("Some Street");
+//        address.setCity("Seoul");
+//        address.setState("Some State");
+//        address.setZipCode("12345");
+
         Member member = new Member();
         member.setUsername("testuser");
+        member.setEmail("testuser@example.com");
         member.setPassword(new BCryptPasswordEncoder().encode("password"));
+        member.setAddress(new HashSet<>());
+        member.setRole(Role.USER);
+        member.setPoints(10000);
+
         memberRepository.save(member);
 
         UserDetails userDetails = userDetailsService.loadUserByUsername("testuser");
+
         assertNotNull(userDetails);
         assertEquals("testuser", userDetails.getUsername());
         assertNotNull(userDetails.getPassword());
+
+        assertTrue(new BCryptPasswordEncoder().matches("password", userDetails.getPassword()));
+
     }
 }
 
