15fcad87efe784867a589381845ee9935cf85f48
ZzinB <mysteleee7@gmail.com>
2024-09-12 12:30:50 +0900

    Feat: Exception
    이메일 중복, 닉네임 중복

---
+++ src/main/java/com/example/Wanted/Market/API/exception/NicknameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.Wanted.Market.API.exception;
+
+public class NicknameAlreadyExistsException extends RuntimeException {
+    public NicknameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
